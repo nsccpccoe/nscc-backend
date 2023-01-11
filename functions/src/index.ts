1c1079462c8612b812fbf4971456f88ffb113691
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-01-11 18:09:30 +0000

    linked express server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
+import * as express from 'express';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+// Create an Express object and routes (in order)
+const app = express();
+
+app.use('/', (req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  res.status(200).send("Hello Express");
 });
+
+// Set our GCF handler to our Express app.
+// http://127.0.0.1:5001/nsccpccoe/asia-south1/events
+export const events = functions.region('asia-south1').https.onRequest(app);
