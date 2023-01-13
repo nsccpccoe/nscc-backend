059345b8d5d7da628be8a5f8c8f3c9777f5239b6
Karteek <ati.karteek99@gmail.com>
2023-01-13 19:57:26 +0530

    Web Dev Hackathon API issues resolved. There are 5 APIs :-
    1. /submit - It requires user id and url in Request Body.
    2. /update - Same as submit.
    3. /page?id={key} - Send PageId in query params.
    4. /all - returns the submitted documents.
    5.  /like - It requires User Id, Post Id in body and will map the like with user id.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,14 @@
 import * as functions from "firebase-functions";
 import * as express from 'express';
+import routes from './router/routes'
+
 
 // Create an Express object and routes (in order)
 const app = express();
 
-app.use('/', (req, res) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  res.status(200).send("Hello Express");
-});
+app.use('/',routes);
+
+
 
 // Set our GCF handler to our Express app.
 // http://127.0.0.1:5001/nsccpccoe/asia-south1/events
