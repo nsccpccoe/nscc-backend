452e1b1cdf3ff535fb0db3c8a34436699cdbb17f
KarteekAti <ati.karteek99@gmail.com>
2023-01-14 11:30:06 +0530

    changed webxplore router from "routes" to "webxplore." changed the router name from something generic as "routes" to more specific name "webxplore". This will help in integrating other APIs on specific routes.

--- functions/src/router/routes.ts
+++ functions/src/router/routes.ts
@@ -1,12 +1,12 @@
-import express = require('express');
-import { submit,getPage,getAll,like,update } from '../controller/apis';
+import express = require("express");
+import { submit, getPage, getAll, like, update } from "../controller/apis";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/submit',submit);
-router.post('/update',update);
-router.get('/page',getPage);
-router.get('/all',getAll);
-router.post('/like',like)
+router.post("/submit", submit);
+router.post("/update", update);
+router.get("/page", getPage);
+router.get("/all", getAll);
+router.post("/like", like);
 
 export default router;
