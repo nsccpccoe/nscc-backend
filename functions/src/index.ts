cc0c8d68249d5acb6faa07f52ae28bc70ce62bfa
Onkar Jadhav <56870381+Omkar76@users.noreply.github.com>
2023-01-14 13:23:22 +0530

    "Implement accounts cloud function"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import webxplore from "./router/routes";
+import * as acc from "./accounts";
 
 // Create an Express object and routes (in order)
 const app = express();
@@ -11,3 +12,6 @@ app.use("/webxplore", webxplore);
 // http://127.0.0.1:5001/nsccpccoe/asia-south1/events
 
 export const events = functions.region("asia-south1").https.onRequest(app);
+export const accounts = functions.region("asia-south1")
+    .https.onRequest(acc.accountsHandler);
+export const saveNewUserToDb = acc.saveNewUserToDb;
