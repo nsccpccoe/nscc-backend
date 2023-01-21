674d6794ae2dc238b07687192bff353b75b253d7
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-01-21 11:25:41 +0530

    added cors

--- functions/src/webxplore/webxplore-service.ts
+++ functions/src/webxplore/webxplore-service.ts
@@ -1,8 +1,10 @@
 import express = require("express");
 import auth from "../middleware/auth";
 import {submit, getSubmissionById, getAllSubmissions, upvote, likedByUser} from "./webxplore-controller";
+import * as cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
