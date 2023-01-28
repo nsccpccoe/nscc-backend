29a52b842927e1c1d28cbd80505796e76ff28302
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-01-28 09:53:33 +0530

    removed unused variables

--- functions/src/accounts/account-events.ts
+++ functions/src/accounts/account-events.ts
@@ -2,6 +2,9 @@ import {region} from "firebase-functions";
 import {firestore} from "firebase-admin";
 
 export const newUserRegistered = region("asia-south1").auth.user().onCreate(async (user) => {
+  // if(!user.emailVerified) {
+  //   auth().generateEmailVerificationLink
+  // }
   return firestore().collection("accounts")
       .doc(user.uid)
       .set({
